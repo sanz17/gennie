ff7484fb7c718a59d04d5a41d26fb871f1528bd2
sanz17 <kundusanhita13@gmail.com>
2023-12-16 10:13:23 +0530

    initial commit

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/hello", (req, res, next) => {
     return res.send("Hello from get");
 });
